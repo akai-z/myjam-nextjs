567de4daac2d8eefb9bcfa5b9c81ca0a59fd8ad4
Akai <akai-z@users.noreply.github.com>
2021-05-08 22:03:48 +0300

    Add Stripe checkout session get serverless function

---
+++ src/functions/get-checkout-session.js
@@ -0,0 +1,18 @@
+const checkoutSession = require('./src/service/stripe-checkout/services/checkout/session')
+const httpMethods = require('./src/common/functions/http-methods')
+const response = require('./src/common/functions/response')
+
+const allowedHttpMethods = ['GET']
+
+exports.handler = async (event, context) => {
+  try {
+    httpMethods.validate(event.httpMethod, allowedHttpMethods)
+
+    const { session_id } = event.query
+    const session = await checkoutSession.get(session_id)
+
+    return response.json({ 'session': session })
+  } catch (err) {
+    return response.error(err)
+  }
+}
