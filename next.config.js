aa3fe9a68b7bcaf49b7f2d4a789ea5b3323bdb5b
Ahmad Kayali <kayali1411@gmail.com>
2021-05-09 12:38:24 +0200

    set target serverless

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withImages = require('next-images');
 
 module.exports = withImages({
   distDir: 'build',
+  target: 'serverless',
   webpack(config, { isServer }) {
     if (!isServer) {
       config.node = { fs: 'empty', module: 'empty' }
