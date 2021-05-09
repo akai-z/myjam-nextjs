c523c384a1c1fd2234be68ea44a863ed21d62487
Akai <akai-z@users.noreply.github.com>
2021-05-10 02:34:30 +0300

    Add catalog config list function

---
+++ src/functions/catalog-config-list.js
@@ -0,0 +1,16 @@
+const config = require('./src/service/catalog/services/config')
+const httpMethods = require('./src/common/functions/http-methods')
+const response = require('./src/common/functions/response')
+
+const allowedHttpMethods = ['GET']
+
+exports.handler = async (event, context) => {
+  try {
+    httpMethods.validate(event.httpMethod, allowedHttpMethods)
+    const configList = await config.list()
+
+    return response.cachedResponse(response.json(configList))
+  } catch (err) {
+    return response.error(err)
+  }
+}
