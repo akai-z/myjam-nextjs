a1bd5c97bfad5881c70107290646dfb7a16bd3f3
Ahmad Kayali <kayali1411@gmail.com>
2021-05-09 11:14:48 +0200

    test netlify-functions

--- next.config.js
+++ next.config.js
@@ -2,7 +2,10 @@
 const withImages = require('next-images');
 
 module.exports = withImages({
-  webpack(config, options) {
+  webpack(config, { isServer }) {
+    if (!isServer) {
+      config.node = { fs: 'empty', module: 'empty' }
+    }
     return config;
   },
 });
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { GetServerSideProps } from 'next';
 
 import Layout from '@components/layout';
 import Blocks from '@components/homepage-blocks';
@@ -6,7 +7,7 @@ import HomeBanner from '@components/homepage-banner';
 import CategorySlider from '@components/category-slider';
 import ItemSlider from '@components/item-slider';
 
-const IndexPage = () => (
+const IndexPage: React.FC = () => (
   <Layout seo={{ title: 'MY JAM' }}>
     <HomeBanner />
     <Blocks />
@@ -15,5 +16,15 @@ const IndexPage = () => (
     <ItemSlider title="Trending Products" type="trending" />
   </Layout>
 );
+
+// @ts-ignore
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const response = await fetch('http://localhost:8888/api/test');
+  const text = await response.text();
+  console.log(text);
+  return {
+    props: { data: text },
+  };
+};
 
 export default IndexPage;
