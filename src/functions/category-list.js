6d9957e87dff8e7c0a7943c66ce7ea2c97d74b7c
Akai <akai-z@users.noreply.github.com>
2021-05-10 02:34:59 +0300

    Add category list function

---
+++ src/functions/category-list.js
@@ -0,0 +1,16 @@
+const category = require('./src/service/catalog/services/category')
+const httpMethods = require('./src/common/functions/http-methods')
+const response = require('./src/common/functions/response')
+
+const allowedHttpMethods = ['GET']
+
+exports.handler = async (event, context) => {
+  try {
+    httpMethods.validate(event.httpMethod, allowedHttpMethods)
+    const categoryList = await category.list()
+
+    return response.cachedResponse(response.json(categoryList))
+  } catch (err) {
+    return response.error(err)
+  }
+}
