d8526a09999afa6c9ddb2359bab0c872a106a160
Akai <akai-z@users.noreply.github.com>
2021-05-10 02:35:28 +0300

    Add product list function

---
+++ src/functions/product-list.js
@@ -0,0 +1,19 @@
+const product = require('./src/service/catalog/services/product')
+const pathParams = require('./src/common/functions/path-params')
+const httpMethods = require('./src/common/functions/http-methods')
+const response = require('./src/common/functions/response')
+
+const allowedHttpMethods = ['GET']
+
+exports.handler = async (event, context) => {
+  try {
+    httpMethods.validate(event.httpMethod, allowedHttpMethods)
+
+    const type = pathParams.param(event.path)
+    const productList = await product.listByType(type)
+
+    return response.cachedResponse(response.json(productList))
+  } catch (err) {
+    return response.error(err)
+  }
+}
