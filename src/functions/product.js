f9fa69e2465aa1ccce92058e6495125ddfca3b84
Akai <akai-z@users.noreply.github.com>
2021-05-10 02:33:52 +0300

    Add product function

---
+++ src/functions/product.js
@@ -0,0 +1,19 @@
+const product = require('./src/service/catalog/services/product')
+const pathParams = require('./src/common/functions/path-params')
+const httpMethods = require('./src/common/functions/http-methods')
+const response = require('./src/common/functions/response')
+
+const allowedHttpMethods = ['GET']
+
+exports.handler = async (event, context) => {
+  try {
+    httpMethods.validate(event.httpMethod, allowedHttpMethods)
+
+    const slug = pathParams.param(event.path)
+    const productData = await product.product(slug)
+
+    return response.cachedResponse(response.json(productData))
+  } catch (err) {
+    return response.error(err)
+  }
+}
