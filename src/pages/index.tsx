717d78e6e1964c341481e764f32005a10ce61e70
Ahmad Kayali <kayali1411@gmail.com>
2021-05-09 11:34:08 +0200

    deploy nextjs app on netlify

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,21 +7,27 @@ import HomeBanner from '@components/homepage-banner';
 import CategorySlider from '@components/category-slider';
 import ItemSlider from '@components/item-slider';
 
-const IndexPage: React.FC = () => (
-  <Layout seo={{ title: 'MY JAM' }}>
-    <HomeBanner />
-    <Blocks />
-    <CategorySlider />
-    <ItemSlider title="Featured Products" type="featured" />
-    <ItemSlider title="Trending Products" type="trending" />
-  </Layout>
-);
+interface Props {
+    data: string;
+}
+
+const IndexPage: React.FC<Props> = ({ data }) => {
+    console.log(data);
+    return (
+        <Layout seo={{ title: 'MY JAM' }}>
+            <HomeBanner />
+            <Blocks />
+            <CategorySlider />
+            <ItemSlider title="Featured Products" type="featured" />
+            <ItemSlider title="Trending Products" type="trending" />
+        </Layout>
+    );
+}
 
 // @ts-ignore
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const response = await fetch('http://localhost:8888/api/test');
+  const response = await fetch('https://practical-dubinsky-3c3f1a.netlify.app/api/test');
   const text = await response.text();
-  console.log(text);
   return {
     props: { data: text },
   };
