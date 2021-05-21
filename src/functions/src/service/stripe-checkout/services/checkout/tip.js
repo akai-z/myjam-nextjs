e1d6e7e6f9e65b7a77b3ffdb0310cf2c692c07a3
Akai <akai-z@users.noreply.github.com>
2021-05-21 23:11:50 +0300

    [stripe-checkout] Rename tipping activation environment variable

--- src/functions/src/service/stripe-checkout/services/checkout/tip.js
+++ src/functions/src/service/stripe-checkout/services/checkout/tip.js
@@ -5,7 +5,7 @@ function amount(amount) {
 }
 
 function isEnabled() {
-  return process.env.TIPPING_ENABLED === 'true'
+  return process.env.CHECKOUT_TIPPING_ENABLED === 'true'
 }
 
 function isTipProduct(productType) {
