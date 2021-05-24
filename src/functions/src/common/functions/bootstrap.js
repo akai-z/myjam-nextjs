f71dc43d824386c88c7bfefc6e43ae5c030ddfd5
Akai <akai-z@users.noreply.github.com>
2021-05-24 22:00:56 +0300

    [catalog] Add path params module to functions common bootstrap module

--- src/functions/src/common/functions/bootstrap.js
+++ src/functions/src/common/functions/bootstrap.js
@@ -2,6 +2,7 @@ require('./config')
 
 module.exports = {
   httpMethods: require('./http-methods'),
+  pathParams: require('./path-params'),
   query: require('./query'),
   responseFactory: require('./response-factory')
 }
