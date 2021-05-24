86a85d702df94a10a8aad4601a22b28f8349d05b
Akai <akai-z@users.noreply.github.com>
2021-05-24 22:12:38 +0300

    [catalog] Use functions bootstrap module in product option list function

--- src/functions/product-option-list.js
+++ src/functions/product-option-list.js
@@ -1,7 +1,5 @@
-require('./src/common/functions/config')
+const { httpMethods, response } = require('./src/common/functions/bootstrap')
 const productOption = require('./src/service/catalog/services/product/option')
-const httpMethods = require('./src/common/functions/http-methods')
-const response = require('./src/common/functions/response')
 
 const allowedHttpMethods = ['GET']
 
