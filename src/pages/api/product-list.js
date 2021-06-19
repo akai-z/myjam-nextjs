447d118a874c926fe4deeb85a098e5b3a90585fb
Akai <akai-z@users.noreply.github.com>
2021-06-20 01:06:13 +0300

    [catalog] Add list all option to Vercel product list serverless function

--- src/pages/api/product-list.js
+++ src/pages/api/product-list.js
@@ -1,17 +1,17 @@
-const { httpMethods, query, responseFactory } = require('../../functions/src/common/functions/bootstrap')
+const { httpMethods, responseFactory } = require('../../functions/src/common/functions/bootstrap')
 const product = require('../../functions/src/service/catalog/services/product')
 
 const allowedHttpMethods = ['GET']
-const requiredParams = ['type']
 
 export default async function handler(req, res) {
   const response = responseFactory.createVercelResponse(res)
 
   try {
     httpMethods.validate(req.method, allowedHttpMethods)
-    query.validate(req.query, requiredParams)
 
-    const productList = await product.listByType(req.query.type)
+    const productList = 'type' in req.query
+      ? await product.listByType(req.query.type)
+      : await product.listAll()
 
     response.json(productList)
   } catch (err) {
