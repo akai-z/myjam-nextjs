495c7485f7590ce7de7e40d786c9ff0002c18dc2
Akai <akai-z@users.noreply.github.com>
2021-06-26 17:21:44 +0300

    [catalog] Rename Algolia admin key environment variable

--- src/functions/src/service/catalog/services/integrations/algolia-factory.js
+++ src/functions/src/service/catalog/services/integrations/algolia-factory.js
@@ -4,7 +4,7 @@ const Algolia = require('./algolia')
 function create(indexName) {
   const client = algoliasearch(
     process.env.NEXT_PUBLIC_ALGOLIA_APP_ID,
-    process.env.NEXT_PUBLIC_ALGOLIA_ADMIN_KEY
+    process.env.FUNCTIONS_ALGOLIA_ADMIN_KEY
   )
 
   return new Algolia(client.initIndex(indexName))
