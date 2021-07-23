5b2a26487f64cb17989bc05182e9f6485bff9875
Akai <akai-z@users.noreply.github.com>
2021-07-23 23:37:21 +0300

    [stripe-checkout] Change Vercel session get serverless function session ID query string to path segment

--- src/pages/api/get-checkout-session/[session].js
+++ src/pages/api/get-checkout-session/[session].js
@@ -1,5 +1,5 @@
-const { httpMethods, responseFactory } = require('../../functions/src/common/functions/bootstrap')
-const checkoutSession = require('../../functions/src/service/stripe-checkout/services/checkout/session')
+const { httpMethods, responseFactory } = require('../../../functions/src/common/functions/bootstrap')
+const checkoutSession = require('../../../functions/src/service/stripe-checkout/services/checkout/session')
 
 const allowedHttpMethods = ['GET']
 
@@ -8,9 +8,7 @@ export default async function handler(req, res) {
 
   try {
     httpMethods.validate(req.method, allowedHttpMethods)
-
-    const { session_id } = req.query
-    const session = await checkoutSession.get(session_id)
+    const session = await checkoutSession.get(req.query.session)
 
     response.json({ 'session': session })
   } catch (err) {
