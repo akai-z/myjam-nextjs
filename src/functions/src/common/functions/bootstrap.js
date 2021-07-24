c497b8f4ba59b0c8ff2d6512f1e1349aab18870a
Akai <akai-z@users.noreply.github.com>
2021-07-24 22:44:08 +0300

    [functions] Add path params mapper module to bootstrap

--- src/functions/src/common/functions/bootstrap.js
+++ src/functions/src/common/functions/bootstrap.js
@@ -4,6 +4,7 @@ module.exports = {
   httpMethods: require('./http-methods'),
   nextAuthSessionFactory: require('./next-auth/session-factory'),
   pathParams: require('./path-params'),
+  pathParamsMapper: require('./path-params/mapper'),
   query: require('./query'),
   requestFactory: require('./request-factory'),
   responseFactory: require('./response-factory')
