3ded2bbb9c3107b5508bba6cdf5d7bfb3e7cb3a3
Akai <akai-z@users.noreply.github.com>
2021-07-25 16:56:17 +0300

    [catalog] Change Vercel product serverless function slug query string to path segment

--- src/pages/api/product/[slug].js
+++ src/pages/api/product/[slug].js
@@ -1,16 +1,13 @@
-const { httpMethods, query, responseFactory } = require('../../functions/src/common/functions/bootstrap')
-const product = require('../../functions/src/service/catalog/services/product')
+const { httpMethods, responseFactory } = require('../../../functions/src/common/functions/bootstrap')
+const product = require('../../../functions/src/service/catalog/services/product')
 
 const allowedHttpMethods = ['GET']
-const requiredParams = ['slug']
 
 export default async function handler(req, res) {
   const response = responseFactory.createVercelResponse(res)
 
   try {
     httpMethods.validate(req.method, allowedHttpMethods)
-    query.validate(req.query, requiredParams)
-
     const productData = await product.product(req.query.slug)
 
     response.json(productData)
