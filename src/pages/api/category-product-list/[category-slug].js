fed8d145d047e411b9bac0ed1565dbdb9dd4e62d
Akai <akai-z@users.noreply.github.com>
2021-07-25 16:58:16 +0300

    [catalog] Change Vercel category product list serverless function slug query string to path segment

--- src/pages/api/category-product-list/[category-slug].js
+++ src/pages/api/category-product-list/[category-slug].js
@@ -1,5 +1,5 @@
-const { httpMethods, query, responseFactory } = require('../../functions/src/common/functions/bootstrap')
-const category = require('../../functions/src/service/catalog/services/category')
+const { httpMethods, responseFactory } = require('../../../functions/src/common/functions/bootstrap')
+const category = require('../../../functions/src/service/catalog/services/category')
 
 const allowedHttpMethods = ['GET']
 const requiredParams = ['category-slug']
@@ -9,7 +9,6 @@ export default async function handler(req, res) {
 
   try {
     httpMethods.validate(req.method, allowedHttpMethods)
-    query.validate(req.query, requiredParams)
 
     const offset = 'offset' in req.query ? req.query.offset : null
     const productList = await category.products(req.query['category-slug'], offset)
