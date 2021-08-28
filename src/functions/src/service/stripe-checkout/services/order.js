414260078046772dc92a2707e87e1384424b7781
Akai <akai-z@users.noreply.github.com>
2021-08-28 22:17:55 +0300

    [stripe-checkout] Replace Airtable order view environment variable with a hardcoded value

--- src/functions/src/service/stripe-checkout/services/order.js
+++ src/functions/src/service/stripe-checkout/services/order.js
@@ -16,7 +16,7 @@ async function create(checkoutSessionId) {
   const promotionCode = await promotionCodePromise
 
   const orderData = new Order(checkout, promotionCode).data
-  const completedOrder = await airtable.createRecord(process.env.CHECKOUT_AIRTABLE_ORDER_VIEW, orderData)
+  const completedOrder = await airtable.createRecord(Order.airtableView, orderData)
 
   await addItems(items, completedOrder.id)
 }
