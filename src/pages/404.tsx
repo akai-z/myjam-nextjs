304f3dfb272dd3021a6eca1501e492f229adff27
Ahmad Kayali <kayali1411@gmail.com>
2021-09-11 22:06:58 +0200

    fix layout types issue

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -4,7 +4,7 @@ import Layout from '@components/layout';
 import NotFound from '@components/not-found';
 
 const NotFoundPage: React.FC = () => (
-  <Layout seo={{ title: 'Not Found', description: 'Page not found' }} isNotFound={true}>
+  <Layout title="Page Not Found" description="Page not found" isNotFound={true}>
     <NotFound />
   </Layout>
 );
