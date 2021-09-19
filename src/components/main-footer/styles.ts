d668ea433f1cbf484041f1d38cfa9d38ebab73f2
Ahmad Kayali <kayali1411@gmail.com>
2021-09-19 11:16:53 +0200

    update footer design

--- src/components/main-footer/styles.ts
+++ src/components/main-footer/styles.ts
@@ -1,7 +1,7 @@
 import tw from 'twin.macro';
 import styled from '@emotion/styled';
 
-export const FooterWrapper = tw.div`w-full bg-gray-800 border-t-4 border-black mx-auto`;
+export const FooterWrapper = tw.div`w-full bg-gray-800 mx-auto`;
 
 export const Wrapper = styled.div`
   ${tw`container mx-auto py-10 px-8 md:py-12 md:px-4 flex flex-col md:flex-row md:justify-between md:items-center`}
@@ -12,7 +12,7 @@ export const Wrapper = styled.div`
 
 export const LinksWrapper = tw.div`flex flex-col mb-12 md:mb-0 space-y-2`;
 
-export const Title = tw.h4`text-white text-lg md:text-2xl mb-1 md:mb-2`;
+export const Title = tw.h4`text-white text-base md:text-lg mb-1 md:mb-2`;
 
 export const RouterLink = tw.a`text-gray-300 hover:text-white hover:underline text-sm md:text-base`;
 
