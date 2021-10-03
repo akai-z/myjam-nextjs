0b5325015248df30c6571da72afcb8ebf541fcbf
Akai <akai-z@users.noreply.github.com>
2021-10-03 16:01:34 +0300

    [catalog] Add proxied product module

---
+++ src/functions/src/service/catalog/services/proxied-product.js
@@ -0,0 +1,50 @@
+const product = require('./product')
+const syncincFactory = require('./integrations/syncinc-factory')
+const HttpError = require('../../../common/error/http')
+
+async function listByIds(ids, pageNumber, pageSize = product.defaultListPageSize) {
+  const filter = `${product.idField} = ANY($4::varchar[])`
+  return await list(pageNumber, pageSize, filter, [ids])
+}
+
+async function listByIdsSize(ids) {
+  const filter = `${product.idField} = ANY($4::varchar[])`
+  return await listSize(filter, [ids])
+}
+
+async function listByType(type, pageNumber, pageSize = product.defaultListPageSize) {
+  validateType(type)
+  return await list(pageNumber, pageSize, type)
+}
+
+async function listByTypeSize(type) {
+  validateType(type)
+  return await listSize(type)
+}
+
+async function list(pageNumber, pageSize = product.defaultListPageSize, filter = '', filterValues = []) {
+  const syncInc = syncincFactory.create()
+  pageSize = pageSize > product.defaultListPageSize ? product.defaultListPageSize : pageSize
+
+  return await syncInc.list(product.tableName, pageNumber, pageSize, filter, filterValues)
+}
+
+async function listSize(filter = '', filterValues = []) {
+  const syncInc = syncincFactory.create(product.idField)
+  return await syncInc.listSize(product.tableName, filter, filterValues)
+}
+
+function validateType(type) {
+  if (!product.validTypes.includes(type)) {
+    throw new HttpError(400, 'Invalid product type')
+  }
+}
+
+module.exports = {
+  listByIds,
+  listByIdsSize,
+  listByType,
+  listByTypeSize,
+  list,
+  listSize
+}
