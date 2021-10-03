20a5117b4435be1679ec72e0bbe0a274d028e89b
Akai <akai-z@users.noreply.github.com>
2021-10-03 16:02:18 +0300

    [catalog] Add proxied products functions to category module

--- src/functions/src/service/catalog/services/category.js
+++ src/functions/src/service/catalog/services/category.js
@@ -19,8 +19,25 @@ async function products(categorySlug, listOffset = null) {
     ? await product.listByIds(categoryData.fields.items, listOffset) : {}
 }
 
+async function proxiedProducts(categorySlug, pageNumber, pageSize = product.defaultListPageSize) {
+  const categoryData = await category(categorySlug)
+  const products = 'items' in categoryData.fields
+    ? await proxiedProduct.listByIds(categoryData.fields.items, pageNumber, pageSize) : {}
+
+  return products
+}
+
+async function proxiedProductsSize(categorySlug) {
+  const categoryData = await category(categorySlug)
+  const size = 'items' in categoryData.fields ? categoryData.fields.items.length : 0
+
+  return { count: size }
+}
+
 module.exports = {
   category,
   list,
-  products
+  products,
+  proxiedProducts,
+  proxiedProductsSize
 }
