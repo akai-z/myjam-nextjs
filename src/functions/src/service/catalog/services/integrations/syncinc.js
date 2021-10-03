4e79602fa833114138d8b212bdc020129dd94a23
Akai <akai-z@users.noreply.github.com>
2021-10-03 16:00:20 +0300

    [catalog] Add Sync Inc module

---
+++ src/functions/src/service/catalog/services/integrations/syncinc.js
@@ -0,0 +1,42 @@
+const HttpError = require('../../../../common/error/http')
+
+class SyncInc {
+  constructor(client, idField = null) {
+    this.client = client
+    this.idField = idField
+  }
+
+  async list(table, pageNumber, pageSize, filter = '', filterValues = []) {
+    const queryValues = [pageSize, pageNumber, pageSize, ...filterValues]
+    const query = `SELECT * FROM public.${table}${this.prepareFilter(filter)} LIMIT $1 OFFSET ($2 - 1) * $3`
+
+    const result = await this.runQuery(query, queryValues)
+
+    return result['rows']
+  }
+
+  async listSize(table, filter = '', filterValues = []) {
+    const queryValues = [...filterValues]
+    const query = `SELECT count(${this.idField}) FROM public.${table}${this.prepareFilter(filter)}`
+
+    const result = await this.runQuery(query, queryValues)
+
+    return result['rows'][0]
+  }
+
+  prepareFilter(filter = '') {
+    return filter ? ` WHERE ${filter}` : filter
+  }
+
+  async runQuery(query, values) {
+    try {
+      this.client.connect()
+      return await this.client.query(query, values)
+    } catch (err) {
+      console.log(err.stack)
+      throw new HttpError(500, 'Failed to run query')
+    }
+  }
+}
+
+module.exports = SyncInc
