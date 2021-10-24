a60c751983236fd83ac8d9cef265eb2c3b09737c
Akai <akai-z@users.noreply.github.com>
2021-10-24 14:51:09 +0300

    [catalog] Use node-postgres pool in PostgreSQL module

--- src/functions/src/service/catalog/services/integrations/syncinc-factory.js
+++ src/functions/src/service/catalog/services/integrations/syncinc-factory.js
@@ -1,22 +1,20 @@
-const ServerlessClient = require('serverless-postgres')
+const { Pool } = require('pg')
 const SyncInc = require('./syncinc')
 
-const clientInfo = {
+const connectionInfo = {
   host: process.env.SYNCINC_HOST || 'evening-soiree.syncincdb.com',
   user: process.env.CATALOG_SYNCINC_PRODUCTS_USER,
   password: process.env.CATALOG_SYNCINC_PRODUCTS_PASSWORD,
   database: process.env.CATALOG_SYNCINC_PRODUCTS_DB,
-  port: process.env.SYNCINC_PORT || 5432,
-  maxConnections: 50,
-  delayMs: 3000
+  port: process.env.SYNCINC_PORT || 5432
 }
 
 function create(idField = null) {
-  return new SyncInc(createClient(), idField)
+  return new SyncInc(createPool(), idField)
 }
 
-function createClient() {
-  return new ServerlessClient(clientInfo)
+function createPool() {
+  return new Pool(connectionInfo)
 }
 
 module.exports = {
