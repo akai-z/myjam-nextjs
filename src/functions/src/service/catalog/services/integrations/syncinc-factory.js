8d17f3212c21b2d79734467f67fe2171125d33c6
Akai <akai-z@users.noreply.github.com>
2021-10-24 14:54:07 +0300

    [catalog] Disable SSL rejectUnauthorized in PostgreSQL connection info

--- src/functions/src/service/catalog/services/integrations/syncinc-factory.js
+++ src/functions/src/service/catalog/services/integrations/syncinc-factory.js
@@ -6,7 +6,10 @@ const connectionInfo = {
   user: process.env.CATALOG_SYNCINC_PRODUCTS_USER,
   password: process.env.CATALOG_SYNCINC_PRODUCTS_PASSWORD,
   database: process.env.CATALOG_SYNCINC_PRODUCTS_DB,
-  port: process.env.SYNCINC_PORT || 5432
+  port: process.env.SYNCINC_PORT || 5432,
+  ssl: {
+    rejectUnauthorized: false
+  }
 }
 
 function create(idField = null) {
