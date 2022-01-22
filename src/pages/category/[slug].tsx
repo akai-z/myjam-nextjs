f3c02473851ba5f8b7040df3511409bfa5f8cf01
Ahmad Kayali <kayali1411@gmail.com>
2022-01-22 23:24:36 +0100

    fix access disabled category

--- src/pages/category/[slug].tsx
+++ src/pages/category/[slug].tsx
@@ -5,6 +5,7 @@ import Layout from '@components/layout';
 import Category from '@components/category';
 import { API_URL } from '@config/env';
 import Loader from '@components/loader';
+import NotFound from '@components/not-found';
 
 type Props = {
   category: Category;
@@ -20,6 +21,14 @@ const CategoryPage: React.FC<Props> = ({ category, records, count, pageSize }) =
     return (
       <Layout title="Myjam - Cultural Grocery" description="We support local">
         <Loader loading={true} size={15} />
+      </Layout>
+    );
+  }
+
+  if (category.fields.status === 'disabled') {
+    return (
+      <Layout title="Page Not Found" description="Page not found" isNotFound={true}>
+        <NotFound />
       </Layout>
     );
   }
